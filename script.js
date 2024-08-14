a6e73cf812481619604f79e4fb8aafa0c53a4e93
Igor <igorm.fonseca@hotmail.com>
2024-08-14 16:49:57 -0300

    structured modal data

--- script.js
+++ script.js
@@ -3,9 +3,19 @@ const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
 
-const modalWrapper = document.querySelector('.modal-wrapper')
-const modalMessage = document.querySelector('.modal .title span')
-const modalBtnClose = document.querySelector('.modal button.close')
+const Modal = {
+
+    wrapper: document.querySelector('.modal-wrapper'),
+    message: document.querySelector('.modal .title span'),
+    buttonClose: document.querySelector('.modal button.close'),
+
+    open() {
+        Modal.wrapper.classList.add('open')
+    },
+    close() {
+        Modal.wrapper.classList.remove('open')
+    }
+}
 
 // 3 maneiras de criar e atribuir função ao evento
 form.onsubmit = function (event) {
@@ -17,8 +27,8 @@ form.onsubmit = function (event) {
     const result = IMC(weight, height)
     const message = `O seu IMC é de ${result}`
 
-    modalMessage.innerText = message
-    modalWrapper.classList.add('open')
+    Modal.message.innerText = message
+    Modal.open()
 }
 
 // or
@@ -29,7 +39,7 @@ form.onsubmit = function (event) {
 // function handleSubmit() {}
 
 
-modalBtnClose.onclick = () => modalWrapper.classList.remove('open')
+Modal.buttonClose.onclick = () => Modal.close()
 
 function IMC(weight, height) {
     return (weight / ((height / 100) ** 2)).toFixed(1)
