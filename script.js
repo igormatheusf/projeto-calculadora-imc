ebf0cf3267a7a88c00dc54120bec35f225b235f9
Igor <igorm.fonseca@hotmail.com>
2024-08-14 16:26:11 -0300

    presentation result in popup

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 // variables
-
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
+
+const modalWrapper = document.querySelector('.modal-wrapper')
+const modalMessage = document.querySelector('.modal .title span')
+const modalBtnClose = document.querySelector('.modal button.close')
 
 // 3 maneiras de criar e atribuir função ao evento
 form.onsubmit = function (event) {
     event.preventDefault()
 
     const weight = inputWeight.value
-    const height = inputWeight.value
+    const height = inputHeight.value
+
+    const result = IMC(weight, height)
+    const message = `O seu IMC é de ${result}`
+
+    modalMessage.innerText = message
+    modalWrapper.classList.add('open')
 }
 
 // or
@@ -18,3 +27,10 @@ form.onsubmit = function (event) {
 // or
 // form.onsubmit = handleSubmit
 // function handleSubmit() {}
+
+
+modalBtnClose.onclick = () => modalWrapper.classList.remove('open')
+
+function IMC(weight, height) {
+    return (weight / ((height / 100) ** 2)).toFixed(1)
+}
