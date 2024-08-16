60f24350ff75eb5bfa7160245de28a62749cb360
Igor <igorm.fonseca@hotmail.com>
2024-08-16 00:06:16 -0300

    refactored submit function

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 import { Modal } from './modal.js'
 import { AlertError } from './alert-error.js'
-import { IMC, notANumber } from './utils.js'
+import { calculateIMC, notANumber } from './utils.js'
 
-// variables
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
@@ -13,16 +12,20 @@ form.onsubmit = function (event) {
     const weight = inputWeight.value
     const height = inputHeight.value
 
-    const showAlertError = notANumber(weight) || notANumber(height)
+    const weightOrHeightIsNotANumber = notANumber(weight) || notANumber(height)
 
-    if (showAlertError) {
+    if (weightOrHeightIsNotANumber) {
         AlertError.open()
         return;
     }
 
     AlertError.close()
 
-    const result = IMC(weight, height)
+    const result = calculateIMC(weight, height)
+    displayResultMessage(result)
+}
+
+export function displayResultMessage(result) {
     const message = `O seu IMC é de ${result}`
 
     Modal.message.innerText = message
